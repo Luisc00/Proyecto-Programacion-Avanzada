bb5d3375a462cadab7a467ae4879f46916882240
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-03-04 11:54:07 -0500

    .

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/interfaces/NegocioServicio.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.proyecto.servicios.interfaces;
+
+public interface NegocioServicio {
+
+    //CRUD
+     void crearNegocio();
+     //fILTRAR
+    //lISTAR PROPIETARIOS
+
+
+}
+
